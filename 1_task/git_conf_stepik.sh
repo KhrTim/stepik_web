6e20562bf1aeac67de51a04a2edcc403367a3851
KhrTim <khairulov00@gmail.com>
2022-06-20 02:52:18 +0300

    ignore added. task 1 removed

--- 1_task/git_conf_stepik.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.email "Khairulov00@gmail.com"                              
-git config --global user.name "KhrTim"
--- 1_task/restart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf;
-sudo rm /etc/nginx/sites-enabled/default;
-
-sudo /etc/init.d/nginx restart;
