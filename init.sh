206b6e336a8a7de9485451195c594d974a2c4038
KhrTim <Khairulov00@gmail.com>
2022-06-19 10:28:55 +0000

    files configured

--- init.sh
+++ init.sh
@@ -10,6 +10,8 @@ mkdir ./public/js;
 
 
 
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf;
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf;
+sudo rm /etc/nginx/sites-enabled/default;
+
 sudo /etc/init.d/nginx restart;
 echo "Existing file linked";
