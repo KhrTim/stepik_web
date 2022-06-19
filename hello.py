ee5b8af432f6dd4b8aafa8a1b0a35b196faeb608
KhrTim <khairulov00@gmail.com>
2022-06-20 02:47:56 +0300

    big changes

---
+++ hello.py
@@ -0,0 +1,13 @@
+def wsgi_processor_make_file(environ, start_response):
+    query = environ["QUERY_STRING"]
+    print(f"RECEIVED {query}")
+    document = query.replace('&', '\n').encode('utf-8') # ??
+
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    status = "200 OK"
+
+    start_response(status, headers)
+    return [document]
+
--- init.sh
+++ init.sh
@@ -12,8 +12,11 @@ mkdir ./public/js;
 mv nginx.conf etc/;
 
 
+gunicorn --bind='0.0.0.0:8080' 'hello:wsgi_processor_make_file()';
+
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf;
 sudo rm /etc/nginx/sites-enabled/default;
 
 sudo /etc/init.d/nginx restart;
-echo "Existing file linked";
+
+
