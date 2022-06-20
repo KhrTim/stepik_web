a0391f678946974fe8d027e4639942cdbab12308
KhrTim <khairulov00@gmail.com>
2022-06-20 20:14:04 +0300

    init changes

--- init.sh
+++ init.sh
@@ -12,11 +12,15 @@ mkdir ./public/js;
 mv nginx.conf etc/;
 
 
-gunicorn --bind='0.0.0.0:8080' 'hello:wsgi_processor_make_file()';
+
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf;
 sudo rm /etc/nginx/sites-enabled/default;
 
 sudo /etc/init.d/nginx restart;
+
+gunicorn --bind='0.0.0.0:8080' hello:wsgi_processor_make_file;
+
+# link gunicorn too
 
 
