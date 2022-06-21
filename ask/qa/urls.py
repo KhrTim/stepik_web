fb923ce13455a557c53c3afad29746c497bf9cf3
KhrTim <khairulov00@gmail.com>
2022-06-21 23:30:25 +0300

    minor changes

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
-urlpatterns = patterns('ask.qa',
-    url(r'^$', 'test', name = 'root'),
-    url(r'^login/$', 'test', name = 'login'),
-    url(r'^signup/$', 'test', name = 'signup'),
-    url(r'^question/(?P<ID>d+)$', 'test', name = 'question'),
-    url(r'^ask/$', 'test', name = 'ask'),
-    url(r'^popular/$', 'test', name = 'popular'),
-    url(r'^new/$', 'test', name = 'new'),
-)
+urlpatterns = [
+    url(r'^$', 'ask.qa.test', name = 'root'),
+    url(r'^login/$', 'ask.qa.test', name = 'login'),
+    url(r'^signup/$', 'ask.qa.test', name = 'signup'),
+    url(r'^question/(?P<ID>d+)$', 'ask.qa.test', name = 'question'),
+    url(r'^ask/$', 'ask.qa.test', name = 'ask'),
+    url(r'^popular/$', 'ask.qa.test', name = 'popular'),
+    url(r'^new/$', 'ask.qa.test', name = 'new'),
+]
