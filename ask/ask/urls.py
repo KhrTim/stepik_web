13ca777a25b96916933902d5c8fad26879b25af3
KhrTim <khairulov00@gmail.com>
2022-06-22 00:03:24 +0300

    minor changes

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -19,4 +19,10 @@ from django.conf.urls import url, include
 urlpatterns = [
     url(r'^admin/$', admin.site.urls),
     url(r'^$', include('qa.urls')),
+    url(r'^login/$', include('qa.urls')),
+    url(r'^signup/$', include('qa.urls')),
+    url(r'^question/(?P<ID>d+)$', include('qa.urls')),
+    url(r'^ask/$', include('qa.urls')),
+    url(r'^popular/$', include('qa.urls')),
+    url(r'^new/$', include('qa.urls')),
 ]
